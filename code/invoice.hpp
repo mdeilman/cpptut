dc9e7ec71fa06d42b815e9a684ad882ada2a8fd2
Mario Deilmann <mario.deilmann@googlemail.com>
2016-09-15 09:53:39 +0200

    Minor gtest and testing additions

--- code/invoice.hpp
+++ code/invoice.hpp
@@ -4,34 +4,33 @@
 #include <iostream>
 #include <string>
 
+double compute_sub_total(double items[], int count){
+
+  double total = 0.0;
+
+  for (int i = 0 ; i < count ; i++){
+    total = total + items[i];
+  }
+  return total;
+}
+
+double apply_discount(double subtotal){
+  return subtotal * (1.0 - 0.05);
+}
+
+double apply_sales_tax(double sub_total){
+  return sub_total * (1.0 + 0.09725);
+}
 
 double compute_invoice(double items[], int count){
 
   double total = 0.0;
 
-  for (int i = 0 ; i < count ; i++){
-    total = total + items[i];
-  }
-
-  total = total * (1.0 - 0.05);
-
-  total = total * (1.0 + 0.09725);
+  total = compute_sub_total(items,count);
+  total = apply_discount(total);
+  total = apply_sales_tax(total);
 
   return total;
 }
-
-
-/*
-double compute_sub_total(double[items], int count){
-
-  double total = 0.0;
-
-  for (int i = 0 ; i < count ; i++){
-    total = total + items[i];
-  }
-
-  return total;
-}
-*/
 
 #endif // INVOICE_HPP_
