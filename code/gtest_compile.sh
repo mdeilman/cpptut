9f40bb1a0ac2a52e11b2d828af7b333a6dcdc6a6
Mario Deilmann <mario.deilmann@googlemail.com>
2017-01-08 15:00:31 +0100

    Changed to new gtest gbench version

--- code/gtest_compile.sh
+++ code/gtest_compile.sh
@@ -1,2 +1,2 @@
-g++ -c unit_$1.cpp -I /Users/mario/src/googletest/googletest/include -I. -I../src -o ../build/unit_$1.o
-g++ -o ../build/unit_$1 ../build/unit_$1.o /Users/mario/src/googletest/build/googlemock/gtest/libgtest.a
+g++ -c unit_$1.cpp -I /Users/mario/src/gtest/googletest/include -I. -I../src -o ../build/unit_$1.o
+g++ -o ../build/unit_$1 ../build/unit_$1.o /Users/mario/src/gtest/build/googlemock/gtest/libgtest.a
